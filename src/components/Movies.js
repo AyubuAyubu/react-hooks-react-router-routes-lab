867b8debe212ea9ebac147504591dc31b2a60d11
AyubuAyubu <ayubu6143@gmail.com>
2022-09-29 12:13:59 +0300

    Done Task

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((everyMovie) => 
+      (
+        <div key={everyMovie.title}>
+          <h2>{everyMovie.title}</h2>
+          <p> Runtime : {everyMovie.time}</p>
+          <ul>
+            {everyMovie.genres.map((genre) => (
+              <li key={genre}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
